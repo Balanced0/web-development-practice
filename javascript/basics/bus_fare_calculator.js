b4a3b43e06594778a32cc63974b3c529a18a17e2
balanced0 <alvichowdhury013@gmail.com>
2026-01-25 14:08:58 +0600

    bus-fare-calculator js practice task

---
+++ javascript/basics/bus_fare_calculator.js
@@ -0,0 +1,13 @@
+let age = 21;
+if(age < 10){
+    console.log("free");
+}
+else if(age >= 10 && age <= 26){
+    console.log("you are a student and will get 50% discount");
+}
+else if(age >= 60){
+    console.log("you will get a 15% discount");
+}
+else{
+    console.log("Regular ticket fare 800 tk");
+}
