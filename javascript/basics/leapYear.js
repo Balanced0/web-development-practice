936038d4339cfb4765179140030230da6072895f
balanced0 <alvichowdhury013@gmail.com>
2026-02-03 11:10:32 +0600

    fixed the leapYear finder

--- javascript/basics/leapYear.js
+++ javascript/basics/leapYear.js
@@ -1,8 +1,8 @@
 function leapYear(year){
-    if (year%100 === 0 && year%4 === 0){
+    if (year%100 !== 0 && year%4 === 0){
         return true;
     }
-    if(year%100 === 0 && year&400 === 0){
+    if(year%400 === 0){
         return true;
     }
     return false;
